66ab93198f65b72a0f83ad3c770b932a9584636c
Ryan Julian <rjulian@usc.edu>
2019-05-10 13:44:57 -0700

    Remove Snapshotter
    This was accidentally included during the import from
    rlworkgroup/garage.
    
    The Snapshotter is not part of the Logger API, so it doesn't really
    belong in this package.

--- src/dowel/__init__.py
+++ src/dowel/__init__.py
@@ -7,15 +7,20 @@ from dowel.logger import Logger, LogOutput
 from dowel.simple_outputs import StdOutput, TextOutput
 from dowel.tabular_input import TabularInput
 from dowel.csv_output import CsvOutput  # noqa: I100
-from dowel.snapshotter import Snapshotter
 from dowel.tensor_board_output import TensorBoardOutput
 
 logger = Logger()
 tabular = TabularInput()
-snapshotter = Snapshotter()
 
 __all__ = [
-    'Histogram', 'Logger', 'CsvOutput', 'StdOutput', 'TextOutput', 'LogOutput',
-    'Snapshotter', 'TabularInput', 'TensorBoardOutput', 'logger', 'tabular',
-    'snapshotter'
+    'Histogram',
+    'Logger',
+    'CsvOutput',
+    'StdOutput',
+    'TextOutput',
+    'LogOutput',
+    'TabularInput',
+    'TensorBoardOutput',
+    'logger',
+    'tabular',
 ]
