f17f8e4a7c0ca97272a10452d6116147e092aaef
Ryan Julian <rjulian@usc.edu>
2019-05-14 11:21:49 -0700

    Bump NEXT_VERSION for v0.0.1 release

--- src/dowel/__version__.py
+++ src/dowel/__version__.py
@@ -2,6 +2,6 @@
 import os
 
 # Update this every release to be 'v{next release number}'
-NEXT_VERSION = 'v0.0.1'
+NEXT_VERSION = 'v0.0.2'
 
 __version__ = os.environ.get('TRAVIS_TAG', '{}-dev'.format(NEXT_VERSION))
