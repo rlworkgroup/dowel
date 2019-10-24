621d3e421f9553dde474d6d6ba82c91ff2da6821
K.R. Zentner <41180126+krzentner@users.noreply.github.com>
2019-10-24 15:34:14 -0700

    Add example (#35)
    I kept forgetting the API, so I figured an example is in order.

---
+++ examples/log_progress.py
@@ -0,0 +1,25 @@
+import time
+
+import dowel
+from dowel import logger, tabular
+
+logger.add_output(dowel.StdOutput())
+logger.add_output(dowel.CsvOutput('progress.csv'))
+logger.add_output(dowel.TextOutput('progress.txt'))
+logger.add_output(dowel.TensorBoardOutput('tensorboard_logdir'))
+
+logger.log('Starting up...')
+for i in range(1000):
+    logger.push_prefix('itr {}: '.format(i))
+    logger.log('Running training step')
+
+    time.sleep(0.01)  # Tensorboard doesn't like output to be too fast.
+
+    tabular.record('itr', i)
+    tabular.record('loss', 100.0 / (2 + i))
+    logger.log(tabular)
+
+    logger.pop_prefix()
+    logger.dump_all()
+
+logger.remove_all()
