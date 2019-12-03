0cd3ff64c64caddb50131f5a74042316ccd877b6
Ryan Julian <ryanjulian@users.noreply.github.com>
2019-12-02 17:39:54 -0800

    Require Python 3.5+ in setup.py (#31)

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
     url='https://github.com/rlworkgroup/dowel',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
+    python_requires='>=3.5',
     install_requires=required,
     extras_require=extras,
     license='MIT',
